c0c7375dab3623340d1d5ef19772f95cbbc5fdd1
Kgl-01 <karthikgowda.mailme@gmail.com>
2025-04-19 23:58:47 +0530

    created new user package which consists of user struct related code

---
+++ user/user.go
@@ -0,0 +1,40 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthDate string
+	age       int
+	createdAt time.Time
+}
+
+func NewUser(firstName string, lastName string, birthDate string, age int) (*User, error) {
+
+	if firstName == "" || lastName == "" || birthDate == "" {
+		return nil, errors.New("firstName, lastName and birthDate is missing")
+	}
+
+	appUser := &User{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		age:       age,
+		createdAt: time.Now(),
+	}
+
+	return appUser, nil
+}
+
+func (u *User) OutputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate, u.createdAt)
+}
+func (u *User) ClearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
